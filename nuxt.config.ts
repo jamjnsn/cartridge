2773d2ecfeb90f3f4385537e7161c94ffcf90ad7
Jamie <jamie@jnsn.me>
2023-04-06 20:32:40 -0700

    Add basic styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 export default defineNuxtConfig({
 	runtimeConfig: {
 		jwtSecret: process.env.JWT_SECRET
+	},
+	css: ['@/assets/styles/main.scss'],
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: '@import "@/assets/styles/_global.scss";'
+				}
+			}
+		}
 	}
 })
