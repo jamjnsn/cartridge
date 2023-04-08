8278f3bdf33f981a844f03c59112b0c087b55d2e
Jamie <jamie@jnsn.me>
2023-04-08 11:18:10 -0700

    Add tooltip component

---
+++ components/ui/UiTooltip.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+withDefaults(
+	defineProps<{
+		label: string
+	}>(),
+	{}
+)
+</script>
+
+<template>
+	<div class="ui-tooltip">
+		<div class="ui-tooltip-content">
+			{{ label }}
+		</div>
+		<slot />
+	</div>
+</template>
+
+<style scoped lang="scss">
+$tooltip-size: 5px;
+
+.ui-tooltip {
+	width: auto;
+	display: inline-flex;
+
+	&:hover {
+		.ui-tooltip-content {
+			opacity: 1;
+			transform: translateY(-100%) translateX(-50%) scale(1);
+		}
+	}
+}
+
+.ui-tooltip-content {
+	opacity: 0;
+	min-width: 50px;
+	text-align: center;
+	z-index: 1;
+	position: absolute;
+	background-color: $primary;
+	padding: 0.3em 0.5em;
+	top: calc(($tooltip-size + 0.35rem) * -1);
+	left: 50%;
+	transform: translateY(-100%) translateX(-50%) scale(0);
+	border-radius: 0.2em;
+	filter: drop-shadow(0 2px 0 darken($primary, 8%));
+	transition: all 0.1s ease;
+
+	&::after {
+		content: '';
+		position: absolute;
+		top: 100%;
+		left: 50%;
+		margin-left: -$tooltip-size;
+		border-width: $tooltip-size;
+		border-style: solid;
+		border-color: $primary transparent transparent transparent;
+	}
+}
+</style>
